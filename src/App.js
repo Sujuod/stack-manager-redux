f40bcafa249043d806719b8f2f61d0c98dea5467
Baha’ almomani <b.almomani@tamhub.com>
2019-09-26 18:15:03 +0400

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Provider } from 'react-redux';
+import { store } from './configureStore';
+import Task from './Task'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <header class="header-fixed">Task Manager with Redux</header>
+      <div>
+        <Task />
+      </div>
+    </Provider>
   );
 }
 
